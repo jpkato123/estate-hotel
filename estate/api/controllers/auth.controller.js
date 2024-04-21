d94031b1dd2d158fccf33607d30f3f2e94f08e05
jpkato <quanghieu.forever@gmail.com>
2024-04-21 13:20:17 +0900

    create signin api route

--- estate/api/controllers/auth.controller.js
+++ estate/api/controllers/auth.controller.js
@@ -1,19 +1,41 @@
-import User from '../models/user.model.js'
-import bcryptjs from 'bcryptjs'
-import { errorHandler } from '../utils/error.js';
-
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-   const {username, email, password} = req.body;
-   const hashedPassword = bcryptjs.hashSync(password,10)
-   const newUser = new User({ username, email,password: hashedPassword });
-   try {
-     await newUser.save();
-     res.status(201).json("user created successfully");
-   } catch (error) {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("user created successfully");
+  } catch (error) {
     // res.status(500).json(error.message)
-    next(error)
+    next(error);
     // next(errorHandler(550,"error from the funciton"));
-   }
-  
-}
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, "wrong credential email or password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass, ...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
